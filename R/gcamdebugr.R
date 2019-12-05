6f78af4dd4ce2935af5cfabfdb2dc5a6494a68d0
Pralit Patel <pralit.patel@pnnl.gov>
2019-12-05 16:22:39 -0500

    Wrap Rcpp generated interface with standard R functions to have a more natural R interface

--- R/gcamdebugr.R
+++ R/gcamdebugr.R
@@ -4,3 +4,55 @@ NULL
 
 #' @importFrom Rcpp loadModule
 loadModule("gcam_module", TRUE)
+
+#' Create GCAM instance
+#' @return GCAM instance
+#' @export
+createAndInitialize <- function() {
+  new(gcam)
+}
+
+#' Run model period
+#' @details This will run the given GCAM instance for all periods up
+#' and including the given period
+#' @param gcam An initialized GCAM instance
+#' @param period The GCAM model period to run up to
+#' @return GCAM instance
+#' @export
+runToPeriod <- function(gcam, period) {
+  gcam$runToPeriod(period)
+
+  gcam
+}
+
+#' Set some aribtrary data into GCAM
+#' @details Use GCAM Fusion to set some table of data into GCAM.
+#' @param gcam An initialized GCAM instance
+#' @param data A data.frame with the data to set
+#' @param path A GCAM fusion-ish search path to determine where to set the data.
+#' @return GCAM instance
+#' @export
+setData <- function(gcam, data, path) {
+  gcam$setData(data, path)
+}
+
+#' Get some arbitrary data out of GCAM
+#' @details Use GCAM Fusion to get some table of data out of GCAM.
+#' @param gcam An initialized GCAM instance
+#' @param path A GCAM fusion-ish search path to determine where to get the data.
+#' @return A tibble containing the requested data
+#' @export
+#' @importFrom dplyr group_by_at vars summarize_at ungroup
+#' @importFrom magrittr %>%
+getData <- function(gcam, path) {
+  data <- gcam$getData(path)
+  # The data comming out of gcam is unaggregated so we will need to do that now
+  # first figure out what the "value" column is, group by everything else, and summarize
+  col_names <- names(data)
+  value_col <- ifelse(col_names[length(col_names)] == "year", col_names[length(col_names)-1], col_names[length(col_names)])
+  group_cols <- col_names[col_names != value_col]
+  data %>%
+    group_by_at(vars(group_cols)) %>%
+    summarize_at(vars(value_col), list(sum)) %>%
+    ungroup()
+}
