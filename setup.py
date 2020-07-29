f8a2a012b23beadc1e9e6bf240910a48b5a27e2f
crvernon <chrisrvernon@gmail.com>
2020-07-29 12:16:34 -0400

    initial setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='gcamdebugr',
+    version='0.1.0',
+    packages=find_packages(),
+    url='https://stash.pnnl.gov/scm/jgcri/gcamdebugr.git',
+    license='BSD 2-Clause',
+    author='Pralit Patel, Chris R. Vernon',
+    author_email='pralit.patel@pnnl.gov, chris.vernon@pnnl.gov',
+    description='Python API for GCAM',
+    python_requires='>=3.6.*, <4'
+)
